ef4e6ad3224f413efdf0d7d9bfeb9359f834c5f9
CraigLovelock <craiglovelock54@hotmail.co.uk>
2014-11-13 23:19:19 +0000

    swd

--- app/views/maintemplate.blade.php
+++ app/views/maintemplate.blade.php
@@ -5,9 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ isset($pageTitle) ? $pageTitle : 'BuildSanctuary' }}</title>
-    <link rel="stylesheet" href="/public_html/css/main.css" />
+    <link rel="stylesheet" href="/css/main.css" />
     <link rel="stylesheet" href="http://getbootstrap.com/examples/sticky-footer-navbar/sticky-footer-navbar.css" />
-    <link rel="stylesheet" href="/public_html/css/custom.css" />
+    <link rel="stylesheet" href="/css/custom.css" />
     <meta name="_token" content="{{ csrf_token() }}"/>
 </head>
 <body>
