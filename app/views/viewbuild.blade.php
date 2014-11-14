1a47350f6edf4c5d85c977bd24ad82b9ceff549d
CraigLovelock <craiglovelock54@hotmail.co.uk>
2014-11-14 22:46:37 +0000

    dfw

---
+++ app/views/viewbuild.blade.php
@@ -0,0 +1,84 @@
+@extends('maintemplate')
+
+@section('body')
+
+<div class="build-information">
+  <div class="build-title">{{ $build->blogtitle }}</div>
+  <button class="btn btn-primary new-post-btn" data-toggle="modal" data-target="#myModal">
+    <!--@if (Auth::user()->id == $build->build_creator_id)-->
+    <span class="glyphicon glyphicon-pencil"></span>
+    <!--@endif-->
+  </button>
+</div>
+
+@section('scripts')
+
+<script>
+$(document).ready(function() 
+{
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+     var st = $(this).scrollTop();
+     if (st > lastScrollTop){
+        $(".navbar").stop(true, false)
+        .animate({ 'marginTop': '-50px'}, 200);
+        $(".build-information").stop(true, false)
+          .animate({ 'marginTop': '0'}, 200)
+          .animate({ 'opacity': '0.8'}, 200)
+          .css('backgroundColor', 'white')
+          .css('position', 'fixed')
+          .css('width', '100%')
+          .css('left', '0')
+          .css('top', '0px')
+          .css('text-align', 'center')
+     } else {
+        $(".navbar").stop(true, false)
+          .animate({ 'marginTop': '0'}, 200);
+        $(".build-information").stop(true, false)
+          .animate({ 'marginTop': '50px'}, 200);
+     }
+     lastScrollTop = st;
+  });
+
+  var $win = $(window);
+  $win.scroll(function () {
+    if ($win.scrollTop() == 0) {
+      $(".build-information")
+        .animate({ 'marginTop': '0'}, 00)
+        .css('backgroundColor', '')
+        .css('position', '')
+        .css('text-align', 'left')
+        .css('top', '');
+    }
+  });
+
+    $('.newupdateform').submit(function() {
+      $(".submit-newupdate-btn").addClass('disabled');
+      $.ajax({
+          url: '/laravelcms/public_html/createpostaction',
+          type: 'post',
+          cache: false,
+          dataType: 'json',
+          data: $('.newupdateform').serialize(),
+          beforeSend: function() {
+            $(".modal-error-message").remove();
+            $(".submit-newupdate-btn").removeClass('disabled');
+          },
+          success: function(data) {
+            if(data.errors) {
+              $('.modal-body').append('<div class="alert alert-danger centre-text modal-error-message" role="alert"><strong>Error!</strong> '+ data.errors +'</div>'); 
+            } else if (data.success) {
+              location.href='/laravelcms/public/viewbuild/'+data.buildID+'/'+data.URLSlug+'?page='+data.lastPage;
+            }
+          },
+          error: function(xhr, textStatus, thrownError) {
+              alert('Something went to wrong.Please Try again later...');
+          }
+      });
+      return false;
+    });
+
+});
+</script>
+
+@stop
