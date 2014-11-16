a1558b78ba559e0c4fb93efadc108670eec33408
Craig Lovelock <craiglovelock54@hotmail.co.uk>
2014-11-16 22:56:45 +0000

    added production enviro

---
+++ app/config/production/database.php
@@ -0,0 +1,49 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Database Connections
+	|
+	|--------------------------------------------------------------------------
+	|
+	| Here are each of the database connections setup for your application.
+	| Of course, examples of configuring each database platform that is
+	| supported by Laravel is shown below to make development simple.
+	|
+	|
+	| All database work in Laravel is done through the PHP PDO facilities
+	| so make sure you have the driver for your particular database of
+	| choice installed on your machine before you begin development.
+	|
+	*/
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => '10.168.1.45',
+			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+			'database'  => 'buildsan_2014',
+			'username'  => 'buildsan_admin',
+			'password'  => '+W!q ]3%{)3_-.p',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+		'pgsql' => array(
+			'driver'   => 'pgsql',
+			'host'     => 'localhost',
+			'database' => 'homestead',
+			'username' => 'homestead',
+			'password' => 'secret',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+			'schema'   => 'public',
+		),
+
+	),
+
+);
