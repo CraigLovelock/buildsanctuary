ded306fc8467d91ce175898bb0bdef53add63b34
Craig Lovelock <craiglovelock54@hotmail.co.uk>
2014-11-16 23:55:51 +0000

    wdd

---
+++ app/views/passwordreset.blade.php
@@ -0,0 +1,18 @@
+@extends('maintemplate')
+
+@section('body')
+
+  <form action="{{ action('RemindersController@postReset') }}" method="POST">
+    <input type="hidden" name="token" value="{{ $token }}">
+    <input type="email" name="email">
+    <input type="password" name="password">
+    <input type="password" name="password_confirmation">
+    <input type="submit" value="Reset Password">
+</form>
+@stop
+
+@section('scripts')
+  <script>
+
+  </script>
+@stop
