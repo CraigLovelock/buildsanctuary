0697fd0ed281b38a59ba491335ad1a356b1ad8c1
Craig Lovelock <craiglovelock54@hotmail.co.uk>
2014-11-17 00:05:56 +0000

    wfwfw

--- app/views/passwordreset.blade.php
+++ app/views/passwordreset.blade.php
@@ -2,13 +2,47 @@
 
 @section('body')
 
-  <form action="{{ action('RemindersController@postReset') }}" method="POST">
-    <input type="hidden" name="token" value="{{ $token }}">
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <input type="password" name="password_confirmation">
-    <input type="submit" value="Reset Password">
-</form>
+{{ Form::open(array('action' => 'RemindersController@postReset', 'class' => 'small-form modal-white')) }}
+
+	<h3 class="no-top-margin">Request Reset</h3>
+
+	@if(Session::has('status'))
+	<div class="alert alert-success" role="alert">
+	  <span class="glyphicon glyphicon-ok"></span>
+	    Success, Check your email.
+	</div>
+	@endif
+
+	{{ Form::hidden('token', $token) }}
+
+	{{ Form::text('email', Input::old('email'), array(
+		'class' => 'form-control',
+		'placeholder' => 'Enter your email'
+	)) }}
+	@if($errors->has('email'))
+		<p class="input-message input-error full-width">{{ $errors->first('email') }}</p>
+	@endif
+
+	{{ Form::password('password', array(
+		'class' => 'form-control',
+		'placeholder' => 'New Password'
+		)) }}
+	@if($errors->has('password'))
+		<p class="input-message input-error full-width">{{ $errors->first('password') }}</p>
+	@endif
+
+	{{ Form::password('password_confirmation', array(
+		'class' => 'form-control',
+		'placeholder' => 'Confirm New password'
+	)) }}
+	@if($errors->has('password_confirmation'))
+		<p class="input-message input-error full-width">{{ $errors->first('password_confirmation') }}</p>
+	@endif
+
+    {{ Form::submit('Reset Password', array('class' => 'btn btn-success full-width')) }}
+
+    {{ Form::close() }}
+
 @stop
 
 @section('scripts')
